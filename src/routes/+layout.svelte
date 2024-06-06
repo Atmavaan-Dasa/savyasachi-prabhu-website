d93f3c4269dd76e05b0bbbae7e55f6395f2212d7
Kallz02 <akshay.pranav.kalathil@gmail.com>
2024-06-06 15:32:32 +0530

    Fix loading issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
 	import '../app.css';
-	import Footer from '../lib/Footer.svelte';
+	
 	import Header from '../lib/Header.svelte';
 </script>
 
 <div class="relative">
 	<Header />
 	<slot />
-	<Footer />
+
 </div>
